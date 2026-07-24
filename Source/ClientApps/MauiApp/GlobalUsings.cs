7ea719d6b25a7ef3253fd9bd6524b6e8a8ee94a4
soroshsabz <soorosh_abi@hotmail.com>
2026-07-24 12:51:05 +0330

    Remove bad project name

--- Source/ClientApps/MauiApp/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using MauiApp.Data;
-global using MauiApp.PageModels;
-global using MauiApp.Pages;
-global using MauiApp.Services;
-global using MauiApp.Utilities;
--- Source/ClientApps/MauiApp/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace MauiApp.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
